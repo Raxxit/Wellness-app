a5d560e3230aaafc94831422de809cb8065207c1
Raxxit <raxxit004@gmail.com>
2026-01-12 17:48:52 +0400

    pls be fixed bro

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'  <-- COMMENT THIS OUT
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
+
+  define: {
+    global: 'window',
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -18,9 +20,5 @@ export default defineConfig({
 
   build: {
     target: 'esnext'
-  },
-
-  define: {
-    global: 'window',
-  },
+  }
 })
