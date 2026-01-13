4d62b2c4a84b44b28b6c088f0c968d24e6b4c2c6
Raxxit <raxxit004@gmail.com>
2026-01-13 16:27:50 +0400

    minor fixes

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Error403 from '@/views/errors/Error403.vue'
 import Error404 from '@/views/errors/Error404.vue'
 import Error405 from '@/views/errors/Error405.vue'
 import Error500 from '@/views/errors/Error500.vue'
+import RegistrationView from '@/views/RegistrationView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,49 +31,49 @@ const router = createRouter({
         {
             path: '/register',
             name: 'register',
-            component: () => import('../views/RegistrationView.vue')
+            component: RegistrationView
         },
 
         {
             path: '/profile',
             name: 'Profile',
-            component: () => import('../views/Profile.vue')
+            component: Profile
         },
 
         {
             path: '/error/400',
             name: 'Error400',
-            component: () => import('@/views/errors/Error400.vue')
+            component: Error400
         },
 
         {
             path: '/error/401',
             name: 'Error401',
-            component: () => import('@/views/errors/Error401.vue')
+            component: Error401
         },
 
         {
             path: '/error/403',
             name: 'Error403',
-            component: () => import('@/views/errors/Error403.vue')
+            component: Error403
         },
 
         {
             path: '/error/404',
             name: 'Error404',
-            component: () => import('@/views/errors/Error404.vue')
+            component: Error404
         },
 
         {
             path: '/error/405',
             name: 'Error405',
-            component: () => import('@/views/errors/Error405.vue')
+            component: Error405
         },
 
         {
             path: '/error/500',
             name: 'Error500',
-            component: () => import('@/views/errors/Error500.vue')
+            component: Error500
         },
     ]
 })
@@ -82,6 +83,3 @@ const router = createRouter({
 
 
 export default router
-
-
-
