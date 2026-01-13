dd5674296cb76b53244b79497e90634ed8e0478f
Raxxit <raxxit004@gmail.com>
2026-01-13 16:03:15 +0400

    fixed minor bug for questionnaire.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
-import QuestionnaireView from '@/views/QuestionnaireView.vue'
+import QuestionnaireView from '@/views/questionnaireView.vue'
 import Profile from '@/views/Profile.vue'
 import Error400 from '@/views/errors/Error400.vue'
 import Error401 from '@/views/errors/Error401.vue'
@@ -14,9 +14,6 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', name: 'home', component: HomeView },
-        // You will create these other files later as you need them
-        // { path: '/community', component: () => import('../views/CommunityView.vue') },
-        // { path: '/login', component: () => import('../views/LoginView.vue') }
 
         {
             path: '/about',
@@ -31,52 +28,52 @@ const router = createRouter({
         },
 
         {
-  path: '/register',
-  name: 'register',
-  component: () => import('../views/RegistrationView.vue')
-},
+            path: '/register',
+            name: 'register',
+            component: () => import('../views/RegistrationView.vue')
+        },
 
-{
-  path: '/profile',
-  name: 'Profile',
-  component: () => import('../views/Profile.vue')
-},
+        {
+            path: '/profile',
+            name: 'Profile',
+            component: () => import('../views/Profile.vue')
+        },
 
-{
-  path: '/error/400',
-  name: 'Error400',
-  component: () => import('@/views/errors/Error400.vue')
-},
+        {
+            path: '/error/400',
+            name: 'Error400',
+            component: () => import('@/views/errors/Error400.vue')
+        },
 
-{
-  path: '/error/401',
-  name: 'Error401',
-  component: () => import('@/views/errors/Error401.vue')
-},
+        {
+            path: '/error/401',
+            name: 'Error401',
+            component: () => import('@/views/errors/Error401.vue')
+        },
 
-{
-  path: '/error/403',
-  name: 'Error403',
-  component: () => import('@/views/errors/Error403.vue')
-},
+        {
+            path: '/error/403',
+            name: 'Error403',
+            component: () => import('@/views/errors/Error403.vue')
+        },
 
-{
-  path: '/error/404',
-  name: 'Error404',
-  component: () => import('@/views/errors/Error404.vue')
-},
+        {
+            path: '/error/404',
+            name: 'Error404',
+            component: () => import('@/views/errors/Error404.vue')
+        },
 
-{
-  path: '/error/405',
-  name: 'Error405',
-  component: () => import('@/views/errors/Error405.vue')
-},
+        {
+            path: '/error/405',
+            name: 'Error405',
+            component: () => import('@/views/errors/Error405.vue')
+        },
 
-{
-  path: '/error/500',
-  name: 'Error500',
-  component: () => import('@/views/errors/Error500.vue')
-},
+        {
+            path: '/error/500',
+            name: 'Error500',
+            component: () => import('@/views/errors/Error500.vue')
+        },
     ]
 })
 
